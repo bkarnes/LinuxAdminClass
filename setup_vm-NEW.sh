aae1c277ad0e3899a270e2239ba5a091c0a83668
Benny Karnes <thedude@blkonline.net>
2024-10-13 16:36:04 -0400

    Fixed some quotes.  Had them in the wrong place.

--- setup_vm-NEW.sh
+++ setup_vm-NEW.sh
@@ -318,11 +318,11 @@ function blackhat-bash(){
     
 echo -e "\n\
 # Black-Hat-Bash:
-alias rustscan='docker run --network=host -it --rm --name rustscan rustscan/rustscan:2.1.1'\n\
-alias gitjacker=\"/home/dude/tools/gitjacker\n\"\
-alias bhb_project=\"cd /media/dude/data/OldKali/Projects/Training/BlackHatBash/Black-Hat-Bash/\n\"\
-alias bhb_labs=\"cd /media/dude/data/OldKali/Projects/Training/BlackHatBash/Black-Hat-Bash/lab/\n\"\
-alias bhb_startlab=\"cd /media/dude/data/OldKali/Projects/Training/BlackHatBash/Black-Hat-Bash/lab/ && docker compose up --detach\n\"\
+alias rustscan=\"docker run --network=host -it --rm --name rustscan rustscan/rustscan:2.1.1\"\n\
+alias gitjacker=\"/home/dude/tools/gitjacker\"\n\
+alias bhb_project=\"cd /media/dude/data/OldKali/Projects/Training/BlackHatBash/Black-Hat-Bash/\"\n\
+alias bhb_labs=\"cd /media/dude/data/OldKali/Projects/Training/BlackHatBash/Black-Hat-Bash/lab/\"\n\
+alias bhb_startlab=\"cd /media/dude/data/OldKali/Projects/Training/BlackHatBash/Black-Hat-Bash/lab/ && docker compose up --detach\"\n\
 alias bhb_stoplab=\"cd /media/dude/data/OldKali/Projects/Training/BlackHatBash/Black-Hat-Bash/lab/ && docker compose down --volumes\"\n"\
 >> ~/.bash_aliases 
 }
