008ac0c6fa93b0dab8f1b82122cbdfa04a358688
Benny Karnes <thedude@blkonline.net>
2024-10-18 14:40:46 -0400

    Added a test to see if Docker already installed.

--- setup_vm-NEW.sh
+++ setup_vm-NEW.sh
@@ -147,21 +147,27 @@ function update-vm(){
 ## Install Docker:
 ##################################################################################
 function install-docker(){
-    # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+   # Test to make sure Docker isn't already on the system:
+   if command -v docker &> /dev/null; then
+       echo "Docker is already installed, skipping installation."
+   else
+      echo "Docker is not installed, installing it now."
+      # Add Docker's official GPG key:
+      sudo apt-get update
+      sudo apt-get install -y ca-certificates curl
+      sudo install -m 0755 -d /etc/apt/keyrings
+      sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+      sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    # Add the repository to Apt sources:
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+      # Add the repository to Apt sources:
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     
-    # Update the APT Cache:
-    sudo apt-get update
+      # Update the APT Cache:
+      sudo apt-get update
     
-    # Install docker and docker-compose components:
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin
+      # Install docker and docker-compose components:
+      sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin
+   fi
 
     # Set the current user to run docker with root previlages:
     echo "Adding the current user to the docker group."
